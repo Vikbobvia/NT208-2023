93083e2510ee3986d5c79b755645f32b6cccbbe2
Vikbobvia <cotdot@kali-host>
2023-09-18 16:01:12 +0700

    Increase text limit

--- Baseline/website/forms.py
+++ Baseline/website/forms.py
@@ -5,7 +5,7 @@ class MyForm(forms.Form):
     # output_field = forms.CharField(label = 'Output', required=False)
     description_input = forms.CharField(
         label='Decription',
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 500px; height: 100px;'}),
+        max_length=30000000000000,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
     )
 
--- Baseline/website/models.py
+++ Baseline/website/models.py
@@ -5,7 +5,7 @@ import datetime
 
 class Text_table(models.Model):
     date = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
+    description = models.TextField(max_length=30000000000000)
     title = models.CharField(default="123", max_length=200)
     id = models.AutoField(primary_key=True)
 
